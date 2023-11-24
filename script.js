c5105f084ba62f25f18be226eb88582f32c28aaf
rozariocjs <sodienrozario@gmail.com>
2023-11-24 21:37:20 +0200

    complete (contact working aswell)

--- script.js
+++ script.js
@@ -22,3 +22,20 @@ function closemenu(){
   sidemenu.style.right = "-200px";
   
 }
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbyKGSoKkFt8FntDxC5Ygf9KMDNQWvgWcBJfK0_PUKu26e0QO2JQg9SCg7llJoQzgcacEQ/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent successfully!"
+        setTimeout(function(){
+          msg.innerHTML = ""
+        }, 5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
